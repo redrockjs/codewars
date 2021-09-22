4222018b0dc822cf6bfe6554e61b95aad817e118
redrockjs <smb.sochi@gmail.com>
2021-09-22 16:25:57 +0300

    docs: start task 35

---
+++ 35_Rsschol-Closures_and_Scopes/index.js
@@ -0,0 +1,37 @@
+function createFunctions(n) {
+    var callbacks = [];
+
+    for (var i=0; i<n; i++) {
+        callbacks.push(function(i) {
+            let num = i
+            return num;
+        });
+    }
+    return callbacks;
+}
+
+var callbacks = createFunctions(5);
+
+console.log(callbacks[1]())
+
+// TASK
+// We want to create a function, which returns an array of functions, which return their
+// index in the array. For better understanding, here an example:
+//
+//     var callbacks = createFunctions(5); // create an array, containing 5 functions
+//
+// callbacks[0](); // must return 0
+// callbacks[3](); // must return 3
+// We already implemented that function, but when we actually run the code, the result doesn't
+// look like what we expected. Can you spot, what's wrong with it? A test fixture is also available
+
+// TEST
+// describe('example tests', function() {
+//     it('functions must return correct number', function() {
+//         var callbacks = createFunctions(5);
+//
+//         for (var i=0; i<callbacks.length; i++) {
+//             Test.assertEquals(callbacks[i](), i, 'Function with index ' + i);
+//         }
+//     });
+// });
