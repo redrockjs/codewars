d6ea126a58e223ca9f22cdeed475106d19c90b7b
redrockjs <smb.sochi@gmail.com>
2021-10-28 08:44:26 +0300

    fix: finish lesson 50

--- 50_Rsschol-Array_Helpers/index.js
+++ 50_Rsschol-Array_Helpers/index.js
@@ -31,10 +31,7 @@ Array.prototype.sum = function () {
 // четный
 Array.prototype.even = function () {
     if (this.length !== 0) {
-        let result = this.map((el, idx) => {
-            if (idx % 2 !== 0) return el
-        })
-        return result.filter(el => el)
+        return this.filter( (el) => 0 == el % 2 )
     } else {
         return 0
     }
@@ -42,24 +39,21 @@ Array.prototype.even = function () {
 // нечетный
 Array.prototype.odd = function () {
     if (this.length !== 0) {
-        let result = this.map((el, idx) => {
-            if (idx % 2 === 0) return el
-        })
-        return result.filter(el => el)
+        return this.filter( (el) => 0 != el % 2 )
     } else {
         return 0
     }
 };
 
 //let numbers = [1, 2, 3, 4, 5];
-let numbers = [];
-
-console.log(numbers.square())  // must return [1, 4, 9, 16, 25]
-console.log(numbers.cube())    // must return [1, 8, 27, 64, 125]
-console.log(numbers.average()) // must return 3)
-console.log(numbers.sum()) // must return 15
-console.log(numbers.even())    // must return [2, 4]
-console.log(numbers.odd())     // must return [1, 3, 5]
+// let numbers = [];
+//
+// console.log(numbers.square())  // must return [1, 4, 9, 16, 25]
+// console.log(numbers.cube())    // must return [1, 8, 27, 64, 125]
+// console.log(numbers.average()) // must return 3)
+// console.log(numbers.sum()) // must return 15
+// console.log(numbers.even())    // must return [2, 4]
+// console.log(numbers.odd())     // must return [1, 3, 5]
 
 //TASK
 //This kata is designed to test your ability to extend the functionality of built-in classes. In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
@@ -96,3 +90,11 @@ console.log(numbers.odd())     // must return [1, 3, 5]
 // Test.assertSimilar(numbers.odd(), [1, 3, 5], 'Wrong result for odd()');
 //   });
 // });
+
+//BEST
+// Array.prototype.square  = function () { return this.map(function(n) { return n*n; }); }
+// Array.prototype.cube    = function () { return this.map(function(n) { return n*n*n; }); }
+// Array.prototype.average = function () { return this.sum() / this.length; }
+// Array.prototype.sum     = function () { return this.reduce(function(a, b) { return a + b; }, 0); }
+// Array.prototype.even    = function () { return this.filter(function(item) { return 0 == item % 2; }); }
+// Array.prototype.odd     = function () { return this.filter(function(item) { return 0 != item % 2; }); }
